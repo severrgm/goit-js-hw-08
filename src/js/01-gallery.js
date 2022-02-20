5add477f7f1b9dac68d5cd973a40810dea9a2610
Siversky Vladimir <rgm.ukraine@gmail.com>
2022-02-20 23:13:56 +0200

    229

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const gallery = galleryItems.map(galleryItem => createGalatyItem(galleryItem));
+
+galleryEl.append(...gallery);
+
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalatyItem({ preview, original, description }) {
+  const link = document.createElement('a');
+  link.classList.add('gallery__item');
+  link.href = original;
+
+  const img = document.createElement('img');
+  img.src = preview;
+  img.alt = description;
+  img.classList.add('gallery__image');
+
+  link.append(img);
+
+  return link;
+}
+
 console.log(galleryItems);
