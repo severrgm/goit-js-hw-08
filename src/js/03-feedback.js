1d320c6471c8a8b20ea094aa2e759ed378076b1e
Siversky Vladimir <rgm.ukraine@gmail.com>
2022-08-07 14:21:46 +0300

    333

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,58 @@
 import throttle from 'lodash/throttle';
 
 const refs = {
-    form: document.querySelector('.feedback-form'),
+  form: document.querySelector('.feedback-form'),
 };
 const LOCAL_KEY = 'feedback-form-state';
 
 addLocalData();
 
 const formData = {
-    email: refs.form.email.value,
-    message: refs.form.message.value,    
-    };
+  email: refs.form.email.value,
+  message: refs.form.message.value,
+};
 
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
-
 function onFormInput(e) {
-    formData[e.target.name] = e.target.value,
-       
+  (formData[e.target.name] = e.target.value),
     localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
-};
+}
 
 function onFormSubmit(e) {
-    e.preventDefault();
-   
-    const dataSubmit = {
-        email: e.currentTarget.email.value,
-        massage: e.currentTarget.message.value,
-    }
-    console.log(dataSubmit);
-    
-    localStorage.removeItem(LOCAL_KEY);
-    refs.form.reset();
-};
+  e.preventDefault();
 
+  const dataSubmit = {
+    email: e.currentTarget.email.value,
+    massage: e.currentTarget.message.value,
+  };
+  console.log(dataSubmit);
+
+  localStorage.removeItem(LOCAL_KEY);
+  refs.form.reset();
+}
+
+// function addLocalData() {
+//      const localData = JSON.parse(localStorage.getItem(LOCAL_KEY))
+
+//     if (!localData) return;
+
+//     if (localData.email)
+//         refs.form.email.value = localData.email;
+//     if (localData.message)
+//         refs.form.message.value = localData.message;
+
+// }
 function addLocalData() {
-     const localData = JSON.parse(localStorage.getItem(LOCAL_KEY))
-    
+  try {
+    const localData = localStorage.getItem(STORAGE_KEY);
     if (!localData) return;
-    
-    if (localData.email)
-        refs.form.email.value = localData.email;        
-    if (localData.message)
-        refs.form.message.value = localData.message;  
-  
+    formData = JSON.parse(localData);
+    for (let key in formData) {
+      form.elements[key].value = formData[key];
+    }
+  } catch (error) {
+    console.log('Get state error: ', error.message);
+  }
 }
