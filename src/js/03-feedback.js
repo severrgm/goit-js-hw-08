7393a0c5140dfb54e5905d148c6f718739d10514
Siversky Vladimir <rgm.ukraine@gmail.com>
2022-08-14 21:19:25 +0300

    111

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,93 +1,34 @@
 import throttle from 'lodash.throttle';
 
-// const refs = {
-//   form: document.querySelector('.feedback-form'),
-// };
-// const LOCAL_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.addEventListener('input', throttle(savedStorage, 500));
 
-// addLocalData();
-
-// const formData = {
-//   email: refs.form.email.value,
-//   message: refs.form.message.value,
-// };
-
-// refs.form.addEventListener('input', throttle(onFormInput, 500));
-// refs.form.addEventListener('submit', onFormSubmit);
-
-// function onFormInput(e) {
-//   (formData[e.target.name] = e.target.value),
-//     localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
-// }
-
-// function onFormSubmit(e) {
-//   e.preventDefault();
-
-//   const dataSubmit = {
-//     email: e.currentTarget.email.value,
-//     massage: e.currentTarget.message.value,
-//   };
-//   console.log(dataSubmit);
-
-//   localStorage.removeItem(LOCAL_KEY);
-//   refs.form.reset();
-// }
-
-// function addLocalData() {
-//      const localData = JSON.parse(localStorage.getItem(LOCAL_KEY))
-
-//     if (!localData) return;
-
-//     if (localData.email)
-//         refs.form.email.value = localData.email;
-//     if (localData.message)
-//         refs.form.message.value = localData.message;
-
-// }
-//
-const form = document.querySelector('.feedback-form');
-const STORAGE_KEY = 'feedback-form-state';
-
-let objectStorage = {
-  email: '',
-  message: '',
-};
-
-getInputFromLS();
-
-function getInputFromLS() {
-  try {
-    const dataLS = localStorage.getItem(STORAGE_KEY);
-    if (!dataLS) return;
-    objectStorage = JSON.parse(dataLS);
-    for (let key in objectStorage) {
-      form.elements[key].value = objectStorage[key];
-    }
-  } catch (error) {
-    console.log('Get state error: ', error.message);
-  }
-}
-
-function onFormInput(e) {
-  objectStorage[e.target.name] = e.target.value.trim();
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(objectStorage));
-}
-
-function onFormSubmit(e) {
-  e.preventDefault();
-
-  const { email, message } = e.currentTarget.elements;
-
-  const formData = {
+function savedStorage() {
+  const { email, message } = feedbackForm.elements;
+  const objJson = JSON.stringify({
     email: email.value,
     message: message.value,
-  };
-
-  console.log('This is Form Data:', formData);
-
-  e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  });
+  localStorage.setItem('feedback-form-state', objJson);
+  console.log(`email: ${email.value},message: ${message.value}`);
 }
 
-form.addEventListener('input', throttle(onFormInput, 500));
-form.addEventListener('submit', onFormSubmit);
+const savedForm = localStorage.getItem('feedback-form-state');
+if (savedForm) {
+  const newObjInfo = JSON.parse(savedForm);
+  const { email, message } = feedbackForm.elements;
+  email.value = newObjInfo.email;
+  message.value = newObjInfo.message;
+}
+
+feedbackForm.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  const { email, message } = feedbackForm.elements;
+  if (email.value === '' || message.value === '') {
+    alert('все поля нужно заполнить');
+    return;
+  }
+  localStorage.removeItem('feedback-form-state');
+  feedbackForm.reset();
+}
